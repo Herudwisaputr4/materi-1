3dfd08ab31bce310167911893ed148bba60a5efd
Herudwisaputr4 <herudwisaputra017@gmail.com>
2024-08-16 18:40:11 +0700

    belajar inheritence

--- index.php
+++ index.php
@@ -10,12 +10,28 @@ class mobil {
 	}
 }
 	
+class bmw extends mobil {
+	
+}
 
-$bmw = new mobil;
+class tesla extends mobil {
+	function selfparking(){
+		echo "parkir otomatis";
+	}
+}
+
+$bmw = new bmw;
 $bmw->merek = "bmw";
 $bmw->tipe = "320i";
 $bmw->mesin ="2000cc";	
 $bmw->max_speed = "280km/h";
+
+$tesla = new tesla;
+$tesla->merek = "tesla";
+$tesla->tipe = "cybertruck";
+$tesla->mesin ="ev";
+$tesla->max_speed ="400km/h";
+
 
 $mercy = new mobil;
 $mercy->merek = "mercy";
@@ -23,20 +39,15 @@ $mercy->tipe = "s350";
 $mercy->mesin ="2000cc";	
 $mercy->max_speed = "280km/h";
 
+
 $kijang = new mobil;
 $kijang->merek = "kijang";
 $kijang->tipe = "reborn";
 $kijang->mesin ="2000cc";	
 $kijang->max_speed = "280km/h";
 
-echo "$mercy->tipe";
-echo "<br>";
-echo "$bmw->tipe";
-echo "<br>";
-echo "$kijang->tipe";
 
-if ($bmw === $kijang) {
-	echo " sama ";
-}else{
-	echo " tidak ";
-}
+
+echo $tesla->kecepatanmaksimal();
+
+
